95e574589118345e03fdb932b96dcc1683123d49
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-12-01 09:03:10 -0600

    My Day 1 submissions

---
+++ Day 1/Part One/D1P1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class D1P1{
+    public static void main(String[] args) throws IOException {
+        File file = new File("Puzzle 1/Inputs.dat");
+        Scanner inputs = new Scanner( file );
+        ArrayList<Integer> nums = new ArrayList<>();
+        while(inputs.hasNextLine()){
+            int num = inputs.nextInt();
+            if(num > 1793){
+                continue;
+            }
+            else {
+                nums.add(num);
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(nums.get(i) + nums.get(j) == 2020){
+                    System.out.println(nums.get(i) * nums.get(j));
+                }
+            }
+        }
+    }
+}
---
+++ Day 1/Part Two/D1P2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class D1P2 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("Puzzle 1/Inputs.dat");
+        Scanner inputs = new Scanner( file );
+        ArrayList<Integer> nums = new ArrayList<>();
+        while(inputs.hasNextLine()){
+            int num = inputs.nextInt();
+            if(num > 2020){
+                continue;
+            }
+            else {
+                nums.add(num);
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                for(int k = 0; k < nums.size(); k++){
+                    if(nums.get(i) + nums.get(j) + nums.get(k) == 2020){
+                        System.out.println(nums.get(i) * nums.get(j) * nums.get(k));
+                    }
+                }
+            }
+        }
+    }
+}
