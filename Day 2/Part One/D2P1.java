79ee0a5dc108c24519adc7f57fe614f4b5b29c7a
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-12-02 15:19:44 -0600

    Day 2 shit

---
+++ Day 2/Part One/D2P1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class D2P1{
+
+    public static boolean validPass(int lower, int upper, String letter, String password) {
+        int count = 0; 
+        for(int i = 0; i < password.length(); i++){
+            if(password.charAt(i) == letter.charAt(0)){
+                count++;
+            }
+        }
+        return (count >= lower && count <= upper);
+    }
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("Inputs.dat");
+        Scanner input = new Scanner(inputFile);
+        int numOfValidPasswords = 0;
+        while(input.hasNext()){
+            String[] parts = input.nextLine().replaceAll(":", "").replaceAll("-", " ").split(" ");
+            numOfValidPasswords += validPass(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), parts[2], parts[3]) ? 1 : 0;           
+        }
+        System.out.println(numOfValidPasswords);
+        input.close();
+
+    }
+}
---
+++ Day 2/Part Two/D2P2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class D2P2{
+
+    public static boolean validPass(int firstLoc, int secondLoc, String letter, String password) {
+        return (password.charAt(firstLoc - 1) == letter.charAt(0) ^ password.charAt(secondLoc - 1) == letter.charAt(0));
+    }
+    public static void main(String[] args) throws IOException {
+        File inputFile = new File("Inputs.dat");
+        Scanner input = new Scanner(inputFile);
+        int numOfValidPasswords = 0;
+        while(input.hasNext()){
+            String[] parts = input.nextLine().replaceAll(":", "").replaceAll("-", " ").split(" ");
+            numOfValidPasswords += validPass(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), parts[2], parts[3]) ? 1 : 0;           
+        }
+        System.out.println(numOfValidPasswords);
+        input.close();
+    }
+}
