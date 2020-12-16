5e3e5b30e3e10892e93d3aac7a636ecd91ce5ac4
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-12-15 18:07:37 -0600

    Day 15 :video_game:

---
+++ Day 15/Part Two/D15P2.py
@@ -0,0 +1,23 @@
+def solve():
+	instr = [int(num) for num in "6,4,12,1,20,0,16".split(",")]
+	
+	speak = {}
+	counter = 1
+	for i in instr[:-1]:
+		speak[str(i)] = counter
+		counter += 1
+	last = str(instr[-1])
+	while counter < 30000000:
+		if last not in speak.keys():
+			speak[last] = counter
+			last = "0"
+			counter += 1
+		else:
+			diff = counter - speak[last]
+			speak[last] = counter
+			last = str(diff)
+			counter += 1
+
+	return last
+
+print(solve())
